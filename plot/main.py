812dcf4a83eabed6ed48c5d732fb4620f907196c
Chris Marsh <chrismarsh.c2@gmail.com>
2021-11-23 14:43:38 -0600

    ensure we setupdask correctly for new pyCHM requirements

--- plot/main.py
+++ plot/main.py
@@ -1,3 +1,4 @@
+import dask
 
 from plot.convert_mesh import *
 from plot.interactive_map import make_map as make_interactive
@@ -5,10 +6,10 @@ from plot.static_map import plot as make_static
 
 
 def main(settings):
-
+    dask.config.set(scheduler='processes')
     print('Converting model output')
     df = filter_output(settings)
-    df = df.chunk({'time':-1,'x':-1,'y':-1})
+    # df = df.chunk({'time':-1,'x':-1,'y':-1})
     make_interactive(settings, df)
 
 
