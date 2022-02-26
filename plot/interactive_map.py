4b59b3746558e9a593253b2868791cd6d6dcff1f
Chris Marsh <chrismarsh.c2@gmail.com>
2022-02-25 20:22:24 -0600

    testing zoom and handle no-postprocesing better

--- plot/interactive_map.py
+++ plot/interactive_map.py
@@ -394,8 +394,8 @@ def get_vmin_vmax(df, var):
 
 def make_map(settings, df):
 
-    minZoom = 1
-    maxZoom = 13
+    minZoom = 11
+    maxZoom = 12
 
     # the lat long in the df isn't /quite/ right but close enough for this
     m = folium.Map(location=[df.lat.mean(), df.lon.mean()], zoom_start=8, tiles=None, control_scale=True ,zoom_control=True, max_zoom=maxZoom, min_zoom=minZoom)
--- plot/main.py
+++ plot/main.py
@@ -1,10 +1,15 @@
 # import dask
 # from plot.convert_mesh import *
 from plot.interactive_map import make_map as make_interactive
-
+import CHM as pc
 
 def main(settings, df):
 
+    if df is None:
+        df = pc.pvd_to_xarray(settings['chm_outpath'],
+                              dxdy=5000,
+                              variables=settings['plot_vars'])
+        df = df.isel(time=[-48, -1])
     make_interactive(settings, df)
 
 
