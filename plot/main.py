8093fcfc3d254a491628ad4a74e78b161b273ee7
Chris Marsh <chrismarsh.c2@gmail.com>
2022-02-25 20:27:28 -0600

    fix dxdy

--- plot/main.py
+++ plot/main.py
@@ -7,7 +7,7 @@ def main(settings, df):
     dask.config.set(scheduler='processes')
     if df is None:
         df = pc.pvd_to_xarray(settings['chm_outpath'],
-                              dxdy=5000,
+                              dxdy=settings['dxdy'],
                               variables=settings['plot_vars'])
         df = df.isel(time=[-48, -1])
     make_interactive(settings, df)
