9366afd36243aa94f6f8a5b080b951bccfa4bd06
Chris Marsh <chrismarsh.c2@gmail.com>
2022-02-25 20:24:46 -0600

    fix processing call

--- plot/main.py
+++ plot/main.py
@@ -1,10 +1,10 @@
-# import dask
+import dask
 # from plot.convert_mesh import *
 from plot.interactive_map import make_map as make_interactive
 import CHM as pc
 
 def main(settings, df):
-
+    dask.config.set(scheduler='processes')
     if df is None:
         df = pc.pvd_to_xarray(settings['chm_outpath'],
                               dxdy=5000,
