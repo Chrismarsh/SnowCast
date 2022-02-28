caa58ca61aa032a3e0bcee92f2fb835bd122e6b4
Chris Marsh <chrismarsh.c2@gmail.com>
2022-02-28 15:53:08 -0600

    set destination files and folders to be 0755 rwx rx rx

--- webupload/upload.py
+++ webupload/upload.py
@@ -18,7 +18,7 @@ def upload(settings):
 
     # -I to ensure that the quick size/time check is skipped and we always upload the tiles
     # --delete to update remote if we remove files locally
-    exec_str = 'rsync -r -I --delete %s/* root@www.snowcast.ca:/var/www/html/v2/' % settings['html_dir']
+    exec_str = 'rsync -r -I --delete --perms --chmod=D07550,F0755 %s/* root@www.snowcast.ca:/var/www/html/v2/' % settings['html_dir']
     subprocess.check_call([exec_str], shell=True)
 
     print('done')
