caac715bec1006101f61c842b2d016b2e2b1481d
Chris Marsh <chrismarsh.c2@gmail.com>
2022-03-23 12:17:03 -0600

    fix upload perms

--- webupload/upload.py
+++ webupload/upload.py
@@ -27,11 +27,11 @@ def upload(settings):
         print(f'Copying {file} for webupload')
         shutil.copy2(file, tiff_path)
 
-    print('Uploading to webhost...', end='')
+    print('Uploading to webhost...')
 
     # -I to ensure that the quick size/time check is skipped and we always upload the tiles
     # --delete to update remote if we remove files locally
-    exec_str = 'rsync -r -I --delete --perms --chmod=D07550,F0755 %s/* root@www.snowcast.ca:/var/www/html/v2/' % settings['html_dir']
+    exec_str = 'rsync -r -I --delete --perms --chmod=0755 %s/* root@www.snowcast.ca:/var/www/html/v2/' % settings['html_dir']
     subprocess.check_call([exec_str], shell=True)
 
     print('done')
