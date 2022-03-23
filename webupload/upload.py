4c8cd37b185d44ab24afddfff44d94a825498f39
Chris Marsh <chrismarsh.c2@gmail.com>
2022-03-23 13:47:56 -0600

    ensure we delete non-empty remote dirs

--- webupload/upload.py
+++ webupload/upload.py
@@ -31,7 +31,7 @@ def upload(settings):
 
     # -I to ensure that the quick size/time check is skipped and we always upload the tiles
     # --delete to update remote if we remove files locally
-    exec_str = 'rsync -r -I --delete --perms --chmod=0755 %s/* root@www.snowcast.ca:/var/www/html/v2/' % settings['html_dir']
+    exec_str = 'rsync -raI --force --delete --perms --chmod=0755 %s/* root@www.snowcast.ca:/var/www/html/v2/' % settings['html_dir']
     subprocess.check_call([exec_str], shell=True)
 
     print('done')
