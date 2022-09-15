421414524ec4b8c5252ad1b7b5515a03b7df0f0b
Chris Marsh <chrismarsh.c2@gmail.com>
2022-09-15 11:34:58 -0600

    add job script

---
+++ postprocess/job.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#SBATCH --time=01:00:00
+#SBATCH --job-name=Snowcast_postprocess
+#SBATCH --nodes=12
+#SBATCH --tasks-per-node=32
+#SBATCH --cpus-per-task=1
+#SBATCH --mem=100G
+
+nt=${SLURM_CPUS_PER_TASK:-1}
+export OMP_NUM_THREADS=$nt
+module load gcc/9.3.0
+module restore snowcast-prod
+
+source /globalhome/cbm038/HPC/venv/snowcast-prod/bin/activate
+cd /globalhome/cbm038/HPC/project/SnowCast
+
+srun --label python MPI_to_tiff.py "$@"
