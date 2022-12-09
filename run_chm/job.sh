1e4bfac0382890d84d4cdfbe77119a07e75e781a
Chris Marsh <chrismarsh.c2@gmail.com>
2022-12-09 16:00:08 -0600

    Force module loaded CHM version

--- run_chm/job.sh
+++ run_chm/job.sh
@@ -8,14 +8,13 @@
 
 # these are controlled from the squeue submit sh script
 #SBATCH --account=hpc_c_giws_prio_clark
-#ignore.SBATCH --account=hpc_c_giws_clark
 
 #SBATCH --mail-user=chris.marsh@usask.ca
 #SBATCH --mail-type=ALL
 
-nt=${SLURM_CPUS_PER_TASK:-1}
-export OMP_NUM_THREADS=$nt
+export OMP_NUM_THREADS=${SLURM_CPUS_PER_TASK:-1}
 module load gcc/9.3.0
-#module load chm/1.2.2
-srun --label --unbuffered  /globalhome/cbm038/HPC/build-chm-perf-malloc/bin/CHM -f config.json.chkp.json
-#srun --label --unbuffered  CHM -f config.json.chkp.json
+module load chm/1.2.6
+
+srun --label --unbuffered  CHM -f config.json.chkp.json
+
