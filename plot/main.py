b7d435784cb211468dd66a141cfe7943244321a9
Chris Marsh <chrismarsh.c2@gmail.com>
2022-12-11 04:30:58 -0600

    use shutil not path to remove the dir

--- plot/main.py
+++ plot/main.py
@@ -1,14 +1,14 @@
 from plot.interactive_map import make_map as make_interactive
-from pathlib import Path
+import shutil
 import os
-
 
 def main(settings, df):
 
     # clean up the old tile files
     tile_path = os.path.join(settings['html_dir'], 'tiles')
 
-    Path(tile_path).unlink(missing_ok=True)
+    shutil.rmtree(tile_path, ignore_errors=True)
+
     os.mkdir(tile_path)
 
     make_interactive(settings, df)
